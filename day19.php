f533a5f84126dc716b6b50b5c8b2de1225142978
Brandon Liang <54skyxenon@gmail.com>
2024-12-19 00:23:18 -0500

    day 19

---
+++ day19.php
@@ -0,0 +1,61 @@
+<?php
+
+use \Ds\Map;
+
+$filename = 'inputs/day19.txt';
+$lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$inventory = explode(', ', $lines[0]);
+$patterns = array_slice($lines, 1);
+
+function dpPart1(array &$inventory, Map &$cache, string $pattern): bool
+{
+    if ($cache->hasKey($pattern)) {
+        return $cache[$pattern];
+    }
+
+    if (in_array($pattern, $inventory)) {
+        return $cache[$pattern] = true;
+    }
+
+    foreach ($inventory as $basePattern) {
+        if (str_starts_with($pattern, $basePattern) and dpPart1($inventory, $cache, substr($pattern, strlen($basePattern)))) {
+            return $cache[$pattern] = true;
+        }
+    }
+
+    return $cache[$pattern] = false;
+}
+
+function dpPart2(array &$inventory, Map &$cache, string $pattern)
+{
+    if ($cache->hasKey($pattern)) {
+        return $cache[$pattern];
+    }
+
+    $ways = 0;
+
+    if (in_array($pattern, $inventory)) {
+        $ways++;
+    }
+
+    foreach ($inventory as $basePattern) {
+        if (str_starts_with($pattern, $basePattern)) {
+            $ways += dpPart2($inventory, $cache, substr($pattern, strlen($basePattern)));
+        }
+    }
+
+    return $cache[$pattern] = $ways;
+}
+
+$part1 = 0;
+$part2 = 0;
+$cacheP1 = new Map();
+$cacheP2 = new Map();
+
+foreach ($patterns as $pattern) {
+    $part1 += dpPart1($inventory, $cacheP1, $pattern);
+    $part2 += dpPart2($inventory, $cacheP2, $pattern);
+}
+
+echo "$part1\n$part2\n";
